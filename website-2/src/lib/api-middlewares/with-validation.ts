cbd85ae7d8bccc8d13a3119876af57db3d9228e5
Anthony Onu <seboh44@gmail.com>
2023-03-24 17:46:01 +0100

    init

--- website-2/src/lib/api-middlewares/with-validation.ts
+++ website-2/src/lib/api-middlewares/with-validation.ts
@@ -1,24 +1,21 @@
-import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
-import * as z from "zod"
-import type { ZodSchema } from "zod"
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+import { ZodError } from 'zod'
+import type { ZodSchema } from 'zod'
 
-export function withValidation<T extends ZodSchema>(
-  schema: T,
-  handler: NextApiHandler
-) {
-  return async function (req: NextApiRequest, res: NextApiResponse) {
-    try {
-      const body = req.body ? req.body : {}
+export function withValidation<T extends ZodSchema>(schema: T, handler: NextApiHandler) {
+	return async function (req: NextApiRequest, res: NextApiResponse) {
+		try {
+			const body = req.body ? req.body : {}
 
-      await schema.parse(body)
+			await schema.parse(body)
 
-      return handler(req, res)
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return res.status(422).json(error.issues)
-      }
+			return handler(req, res)
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return res.status(422).json(error.issues)
+			}
 
-      return res.status(422).end()
-    }
-  }
+			return res.status(422).end()
+		}
+	}
 }
